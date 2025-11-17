4f3615f6539c95e2ca12b259ce3a6e015dab5a7f
FueguitoOutzz <renatochavezz10@gmail.com>
2025-11-17 16:12:09 -0300

    first commit

---
+++ src/main/java/com/mycompany/mvc_recordatorio_medico/Mvc_recordatorio_medico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mvc_recordatorio_medico;
+
+/**
+ *
+ * @author Mizu
+ */
+public class Mvc_recordatorio_medico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
